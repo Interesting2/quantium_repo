a240de0512c4333a89b9d58f723673035f5252ab
MetaCoderX <softwareguyhuron@gmail.com>
2025-04-10 09:44:54 +1000

    added bash script to test code

---
+++ automate_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source venv/bin/activate
+
+pytest
+
+exit_code=$?
+
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+if [[ "$exit_code" -eq 0 ]]; then
+    echo -e "${GREEN}✅ All tests passed successfully!${NC}"
+
+    exit 0
+else
+    echo -e "${RED}❌ Some tests failed!${NC}"
+
+    exit 1
+fi
